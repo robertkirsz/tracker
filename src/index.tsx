9e1cd1cc5bcd21cc8575e8d2f7677dae27236891
Robert Kirsz <robert.kirsz@gmail.com>
2020-12-07 17:49:10 +0100

    Move CSS import to HTML

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 
 import App from 'components/App'
-import reportWebVitals from 'reportWebVitals'
-
-import 'index.css'
+// import reportWebVitals from 'reportWebVitals'
 
 render(
   <StrictMode>
@@ -16,4 +14,4 @@ render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+// reportWebVitals()
